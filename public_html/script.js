6d3a24304286a3fbd49728bd9f477ba18768632f
lesliechu21 <lesliechu21@gmail.com>
2014-11-13 21:54:09 -0800

    Update script.js

--- public_html/script.js
+++ public_html/script.js
@@ -1,11 +1,37 @@
 $(document).ready(function(){
+
+  window.handle_data = function(my_data, where_it_is, destination){
+    
+    for (var i = 0; i < my_data[where_it_is].length; i++) { 
+      var station = my_data[where_it_is][i];
+      var labelclass = "label-success";
+      if (station.statusKey == 0) {
+        labelclass = "label-danger"
+      };
+      $(destination).append(
+        "<tr>" +
+          "<td>" +
+            "<div class='col-md-2'><label class='label " + labelclass + "'>" + station.statusValue + "</label></div>" +
+            "<div class='col-md-4'>" + station.stationName + "</div>" + 
+            "<div class='col-md-1' style='overflow:hidden;'>" + station.latitude + "</div>" +
+            "<div class='col-md-1' style='overflow:hidden;'>" + station.longitude + "</div>" +
+            
+            "<div class='col-md-2'>" + station.availableBikes + "</div>" + 
+            "<div class='col-md-2'>" + station.availableDocks + "</div>" +
+          "</td>" +
+        "</tr>");
+    }
+
+  };
+
   $.ajax({
-		url: "https://students.ics.uci.edu/~chulm/myProxy.php?https://divvy.com/stations/json",
-		dataType: "json",
-		success: function(json){
-		  window.franklin = json;
-		},
-		error: function(json){
-		}
-	});
+    url: "https://students.ics.uci.edu/~chulm/myProxy.php?https://divvy.com/stations/json",
+    dataType: "json",
+    success: function(json){
+      handle_data(json, "stationBeanList", "#info");
+      
+    },
+    error: function(json){
+    };
+  });
 })
