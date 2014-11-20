5c6b12a011720541115e00495bf7f913d0bb3f85
lesliechu21 <lesliechu21@gmail.com>
2014-11-19 19:51:31 -0800

    Update script.js

--- public_html/script.js
+++ public_html/script.js
@@ -1,6 +1,36 @@
 $(document).ready(function(){
+  function initialize() {
+        var mapOptions = {
+          center: { lat: 41.8649334, lng: -87.6523329},
+          zoom: 11
+        };
+        window.map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+
+  window.set_marker = function(lat, lng, stationName, numOfBikes) {
+    var latLng = new google.maps.LatLng(lat,lng);
+    var fillColor = 'green';
+    if(numOfBikes<=3){
+      fillColor = 'red';
+    }
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      title: stationName,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10,
+        fillColor: fillColor
+        }
+    });
+  }
+
+  
 
   window.handle_data = function(my_data, where_it_is, destination){
+    
     
     for (var i = 0; i < my_data[where_it_is].length; i++) { 
       var station = my_data[where_it_is][i];
@@ -12,28 +42,34 @@ $(document).ready(function(){
         "<tr>" +
           "<td>" +
             "<div class='col-md-2'><label class='label " + labelclass + "'>" + station.statusValue + "</label></div>" +
-            "<div class='col-md-4'>" + station.stationName + "</div>" + 
-            "<div class='col-md-1' style='overflow:hidden;'>" + station.latitude + "</div>" +
-            "<div class='col-md-1' style='overflow:hidden;'>" + station.longitude + "</div>" +
-            
+            "<div class='col-md-4'>" + "<a class='oreo' data-lat='" + station.latitude + "' data-long='" + station.longitude + "'>" + station.stationName + "</a>" + "</div>" + 
             "<div class='col-md-2'>" + station.availableBikes + "</div>" + 
             "<div class='col-md-2'>" + station.availableDocks + "</div>" +
           "</td>" +
         "</tr>");
+      
+      set_marker(station.latitude, station.longitude, station.stationName, station.availableBikes);
+          
     }
-
+  $(".oreo").click(function(button){
+    var lat = $(button).data("lat");
+    var lng = $(button).data("lng");
+    map.setCenter({lat: lat, lng: lng});
+    map.setZoom(16);
+  });
+      
   };
 
+
+
   $.ajax({
-    type: "GET",
-    url: "https://students.ics.uci.edu/~chulm/myProxy.php?https://divvy.com/stations/json",
-    async: false,
-    jsonpCallback: 'jsonCallback',
+    url: "https://students.ics.uci.edu/~chulm/myProxy.php?https://divvybikes.com/stations/json",
     dataType: "json",
-    contentType: "application/json",
     success: function(json){
       handle_data(json, "stationBeanList", "#info");
       
+    },
+    error: function(json){
     }
   });
 })
